8fd9cb7556e7b06efbc3a000f6e303b02144d95f
ahmetKeskiinn <ahmetkeskiinnn@outlook.com>
2023-07-23 19:12:55 +0300

    firebase module provides firestore

--- app/src/main/java/com/ahmetkeskin/bitcointicker/di/FirebaseModule.kt
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/di/FirebaseModule.kt
@@ -1,6 +1,7 @@
 package com.ahmetkeskin.bitcointicker.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +17,8 @@ class FirebaseModule {
     ): FirebaseAuth {
         return FirebaseAuth.getInstance()
     }
+
+    @Singleton
+    @Provides
+    fun provideFireStore() = FirebaseFirestore.getInstance()
 }
