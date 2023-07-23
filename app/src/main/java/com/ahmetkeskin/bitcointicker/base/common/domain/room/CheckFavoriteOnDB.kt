0e94b08ed04496932e296c8c781968f203f661a5
ahmetKeskiinn <ahmetkeskiinnn@outlook.com>
2023-07-23 19:12:21 +0300

    check favorite currency in db usecase added

---
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/base/common/domain/room/CheckFavoriteOnDB.kt
@@ -0,0 +1,33 @@
+package com.ahmetkeskin.bitcointicker.base.common.domain.room
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ahmetkeskin.bitcointicker.base.BaseUseCase
+import com.ahmetkeskin.bitcointicker.base.BaseViewModel
+import com.ahmetkeskin.bitcointicker.base.common.data.db.FavoriteModel
+import com.ahmetkeskin.bitcointicker.base.common.data.repo.FavRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class CheckFavoriteOnDB @Inject constructor(
+    private val room: FavRepository
+) : BaseUseCase<MutableLiveData<List<FavoriteModel>?>, CheckFavoriteOnDB.Params>() {
+
+    data class Params(
+        val favName: String
+    )
+
+    override fun execute(
+        viewModel: BaseViewModel,
+        input: Params?
+    ): MutableLiveData<List<FavoriteModel>?> {
+        return MutableLiveData<List<FavoriteModel>?>().apply {
+            input?.let {
+                viewModel.viewModelScope.launch(Dispatchers.IO){
+                    postValue(room.checkFav(name = it.favName))
+                }
+            }
+        }
+    }
+}
