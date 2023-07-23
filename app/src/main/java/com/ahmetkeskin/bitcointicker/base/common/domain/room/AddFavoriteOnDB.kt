59832c541fb00bf07d389300e8d47acb2d4f993c
ahmetKeskiinn <ahmetkeskiinnn@outlook.com>
2023-07-23 19:12:00 +0300

    add favorite currency in db usecase added

---
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/base/common/domain/room/AddFavoriteOnDB.kt
@@ -0,0 +1,29 @@
+package com.ahmetkeskin.bitcointicker.base.common.domain.room
+
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.ahmetkeskin.bitcointicker.base.BaseUseCase
+import com.ahmetkeskin.bitcointicker.base.BaseViewModel
+import com.ahmetkeskin.bitcointicker.base.common.data.db.FavoriteModel
+import com.ahmetkeskin.bitcointicker.base.common.data.repo.FavRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class AddFavoriteOnDB @Inject constructor(
+    private val room: FavRepository
+) : BaseUseCase<Unit, AddFavoriteOnDB.Params>() {
+
+    data class Params(
+        val favoriteModel: FavoriteModel
+    )
+
+    override fun execute(viewModel: BaseViewModel, input: Params?) {
+        input?.let {
+            viewModel.viewModelScope.launch(Dispatchers.IO){
+                room.insertFavorite(it.favoriteModel)
+                Log.d("TAG", "execute: " + room.checkFav(input.favoriteModel.favCoinName).toString())
+            }
+        }
+    }
+}
