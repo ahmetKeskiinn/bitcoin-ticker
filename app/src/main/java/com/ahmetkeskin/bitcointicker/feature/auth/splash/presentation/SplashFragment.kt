f7be217299e05a36abd9a4de3dde2cc7d8376b9d
ahmetKeskiinn <ahmetkeskiinnn@outlook.com>
2023-07-23 05:18:48 +0300

    splash viewmodel changed for usecases

--- app/src/main/java/com/ahmetkeskin/bitcointicker/feature/auth/splash/presentation/SplashFragment.kt
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/feature/auth/splash/presentation/SplashFragment.kt
@@ -1,15 +1,14 @@
 package com.ahmetkeskin.bitcointicker.feature.auth.splash.presentation
 
+import android.content.Intent
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import com.ahmetkeskin.bitcointicker.MainActivity
 import com.ahmetkeskin.bitcointicker.R
 import com.ahmetkeskin.bitcointicker.base.BaseFragment
-import com.ahmetkeskin.bitcointicker.components.TickerButtonClicked
-import com.ahmetkeskin.bitcointicker.components.buttonattributes.ButtonAttributes
-import com.ahmetkeskin.bitcointicker.components.buttonattributes.Gravity
-import com.ahmetkeskin.bitcointicker.components.buttonattributes.TickerButtonAttributes
-import com.ahmetkeskin.bitcointicker.components.buttonattributes.TickerButtonTextAttributes
 import com.ahmetkeskin.bitcointicker.databinding.FragmentSplashBinding
+import com.ahmetkeskin.bitcointicker.feature.auth.splash.data.response.UserModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,5 +19,37 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(
         activity?.let {
             viewModel = ViewModelProvider(it)[SplashViewModel::class.java]
         }
+        initUI()
+    }
+
+    private fun initUI() {
+        viewModel.getUserSettings().observe(viewLifecycleOwner, Observer {
+            if (it.password != null || it.email != null) {
+                changeFlowForAuthentication(UserModel(email = it.email, password = it.password))
+            } else {
+                goLogin()
+            }
+        })
+    }
+
+    private fun changeFlowForAuthentication(userModel: UserModel) {
+        viewModel.auth(userModel).observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                goHome()
+            } else {
+                goLogin()
+            }
+        })
+    }
+
+    private fun goLogin() {
+        Navigation.findNavController(binding.root)
+            .navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    private fun goHome() {
+        val intent = Intent(activity, MainActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
     }
 }
--- app/src/main/java/com/ahmetkeskin/bitcointicker/feature/auth/splash/presentation/SplashViewModel.kt
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/feature/auth/splash/presentation/SplashViewModel.kt
@@ -1,13 +1,24 @@
 package com.ahmetkeskin.bitcointicker.feature.auth.splash.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ahmetkeskin.bitcointicker.base.BaseViewModel
-import com.ahmetkeskin.bitcointicker.feature.home.domain.GetCryptos
+import com.ahmetkeskin.bitcointicker.feature.auth.splash.data.response.UserModel
+import com.ahmetkeskin.bitcointicker.feature.auth.splash.domain.Authentication
+import com.ahmetkeskin.bitcointicker.feature.auth.splash.domain.GetLocalSettings
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val getCryptos: GetCryptos
+    private val getLocalSettings: GetLocalSettings,
+    private val authentication: Authentication
 ) : BaseViewModel() {
-    fun getCrypto() = getCryptos.execute(this, Unit)
+    private var _getAuthenticationResponse = MutableLiveData<Boolean?>()
+    val authenticationResponse: LiveData<Boolean?> =
+        _getAuthenticationResponse
+    fun getUserSettings() = getLocalSettings.execute(this, Unit)
+
+    fun auth(userModel: UserModel)= authentication.execute(this, Authentication.Params(userModel))
+
 }
