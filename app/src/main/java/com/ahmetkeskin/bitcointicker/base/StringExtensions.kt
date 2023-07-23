8a88d7dd38c3880041888c0f3391f11913507bfc
ahmetKeskiinn <ahmetkeskiinnn@outlook.com>
2023-07-23 19:10:38 +0300

    strings extensions added

--- app/src/main/java/com/ahmetkeskin/bitcointicker/base/StringExtensions.kt
+++ app/src/main/java/com/ahmetkeskin/bitcointicker/base/StringExtensions.kt
@@ -3,3 +3,9 @@ package com.ahmetkeskin.bitcointicker.base
 const val EMPTY: String = ""
 const val DATE_SUFFIX: String = "T00:00:00"
 
+const val FAVORITE_CURRENCY = "favoriteCurrency"
+const val FAVORITE_CURRENCY_RATE = "favoriteRate"
+
+const val USD = "USD"
+
+
